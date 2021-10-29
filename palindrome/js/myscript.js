8fa2f53b7fdb4e001b0555e9485e858395b855df
Giuseppe0197 <faro.g@hotmail.it>
2021-10-29 18:50:45 +0200

    modifiche

--- palindrome/js/myscript.js
+++ palindrome/js/myscript.js
@@ -5,31 +5,29 @@ Dare l’output relativo. */
 
 /* creiamo la variabile per far stampare in pagina il risultato */
 
-let risultatoParola = document.getElementById("risultato");
+let wordResult = document.getElementById("risultato");
 
 /* creiamo un prompt per far inserire all'utente una parola */
 
-const string = prompt("inserisci una parola per verificare che sia palindroma oppure no");
+const insertedWord = prompt("inserisci una parola per verificare che sia palindroma oppure no");
 
 /* creiamo una funzione che stabilisce se la parola scritta dall'utente è palindroma oppure no */
 
 function palindrome(){
 
-    const parole = string.length;
-
     /* creiamo un ciclo per verificare se la parola è palindroma oppure no */
 
-    for (let i = 0; i < parole; i++) {
+    for (let i = 0; i < insertedWord.length / 2; i++) {
 
         /* stabiliamo se la parola è palindroma */
 
-        if (string[i] != string[parole - 1 -i]){
+        if (insertedWord[i] !== insertedWord[insertedWord.length - 1 -i]){
 
             /* andiamo a stampare sulla pagina il risultato */
 
-            risultatoParola.innerHTML = `Non è palindroma`;
+            wordResult.innerHTML = `Non è palindroma`;
 
-            return parole;
+            return insertedWord.length;
 
         }
 
@@ -37,16 +35,16 @@ function palindrome(){
 
     /* andiamo a stampare sulla pagina il risultato */
 
-    risultatoParola.innerHTML = `è palindroma`;
+    wordResult.innerHTML = `è palindroma`;
 
-    return parole;
+    return insertedWord.length;
 
 }
 
 /* creiamo una costante che abbia il valore della funzione */
 
-const verifica = palindrome(string);
+const verify = palindrome(insertedWord);
 
 /* facciamo un console log per verificare */
 
-console.log(verifica);
+console.log(verify);
